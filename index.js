bbd5d758875531a8c57ce2db89d7542951d9c31e
Neil <unnhao@gmail.com>
2019-10-21 00:25:51 +0800

    done

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const
 
 
 // Sets server port and logs message on success
-app.listen(process.env.PORT || 5000, () => console.log('webhook is listening'));
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
 
 // Creates the endpoint for our webhook 
